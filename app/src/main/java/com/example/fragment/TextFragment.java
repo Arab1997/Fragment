14a5be52d568501c3bdedbf76adeccf1c2047050
Abdulloh <maxmudovabdullo97@gmail.com>
2020-07-21 13:18:08 +0500

    Lesson6

---
+++ app/src/main/java/com/example/fragment/TextFragment.java
@@ -0,0 +1,32 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TextFragment extends Fragment {
+    TextView text,vers;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_text, container, false);
+
+        text= (TextView) view.findViewById(R.id.AndroidOs);
+        vers= (TextView)view.findViewById(R.id.Version);
+
+
+        return view;
+
+    }
+    public void change(String txt, String txt1){
+        text.setText(txt);
+        vers.setText(txt1);
+
+    }
+    }
